d1ccd8fa8adc52ed874e3eff947519a8332a3e4f
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-08-09 18:58:56 +0000

    p1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+
 
 const App = () => {
+  let [data,setData]=useState({name:"",email:"",password:""});
+  function handlethis(event) {
+    event.preventDefault();
+    console.log(data);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <form onSubmit={handlethis}>
+          <label htmlFor="naming">Name</label>
+          <input type="text" id="naming" value={data.name} onChange={(e)=>{setData({...data,name:e.target.value})}}/>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" value={data.email} onChange={(e)=>{setData({...data,email:e.target.value})}}/>
+          <label htmlFor="pass">Password</label>
+          <input type="password" id="pass" value={data.password} onChange={(e)=>{setData({...data,password:e.target.value})}}/>
+          <button type="submit">Submit</button>
+        </form>
     </div>
   )
 }
